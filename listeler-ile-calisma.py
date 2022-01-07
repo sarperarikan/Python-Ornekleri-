ac15974e44e6e7c3c130a7eec5a5dd565b776ed3
sarperarikan <sarperarikan@gmail.com>
2022-01-07 09:10:20 +0300

    Add files via upload

---
+++ listeler-ile-calisma.py
@@ -0,0 +1,43 @@
+#Bir liste oluşturmak için değişken = [] şeklinde tanımlayabiliriz.
+#Liste oluşturulurken liste öğeleri arasında virgül konur. Farklı türden veriler listede yer alabilir.
+
+arabalar = ["bmw","ford","fiat"]
+sayilar = [10,20,30]
+#print(arabalar[0])
+#print(arabalar[0:-1])
+#print(arabalar[1:])
+#Öğeleri ters çevirmek için aşağıdaki yöntemi kullanabilirsiniz. Farklı
+#print(arabalar[::-1])
+
+
+#arabalar.append("ferrari")
+#arabalar.insert(0,"jaguar")
+#print(arabalar)
+
+#Liste öğelerinin sonuna veri eklemek için append istenilen noktaya veri eklemek için insert kullanılır. Append içerisinde sadece verinin kendisi parametre olarak atanır. Insert fonksiyonunda ise önce atanılmak istenen sıra numarası belirtilir ve sonrasında veri tanımlanır.
+
+#Bir öğenin verisini değiştirmek için o öğenin sıra numarasını aşağıdaki gibi yazarız.
+
+#arabalar[2] = "şahin"
+#print(arabalar)
+
+#Listeden bir öğe silmek için remove ve pop fonksiyonları kullanılır. Bununla birlikte tamamen listeyi kaldırmak için del kullanılabilir.
+
+#arabalar.pop()
+#print(arabalar)
+#arabalar.remove("ford")
+#arabalar)
+#arabalar.pop(0)
+#print(arabalar)
+
+#Liste öğelerinden herhangi bir istediğimizi silmek içinde pop fonksiyonunu kullanabiliriz. Yapmamız gereken yukarıdaki gibi pop içinde ilgili öğenin indis numarasını belirtmemiz olacaktır. 
+ 
+
+
+
+
+
+
+
+
+input()
