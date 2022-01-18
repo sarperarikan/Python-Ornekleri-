ee1ea8b0ee074fc10244db5bf6f26e09f8b9c281
sarperarikan <sarperarikan@gmail.com>
2022-01-18 16:05:40 +0300

    Add files via upload

---
+++ demetlere-ornek.py
@@ -0,0 +1,12 @@
+#Demetler değiştirilemeyen veri türleridir. 
+yazilim_dilleri = ("php","html","python")
+#print(yazilim_dilleri[1])
+#Yazılım dilleri adlı tuple veri türündeki değişkenin herhangi bir elemanına ulaşmak için o elemanın index numarasını yazdık.
+#for i in yazilim_dilleri:
+ #   print(i,sep="\n")
+#Demetlere for döngüsüylede erişilebilinir.
+#max ve min fonksiyonlarını burada kullanabilirsiniz. max en yüksek değeri min ise en düşük değeri bulur.
+#Bununla birlikte len ve count fonksiyonlarınıda kullanabilirsiniz. len demet içindeki toplam öğe sayısını verirken, count ise bir öğenin kaç defa geçtiğini bulmaktadır.  
+
+
+input()
