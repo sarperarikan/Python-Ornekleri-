1718bd542a6c2a0a34fed68ccfef69376c299535
sarperarikan <sarperarikan@gmail.com>
2022-01-25 14:47:39 +0300

    Add files via upload

---
+++ sozlukler.py
@@ -0,0 +1,20 @@
+#Bir sözlük oluşturmak için değişken adından sonraki gelen kısımda aç büyük parantez ve kapa büyük parantez kullanılır.
+
+plakalar = {'07':'Antalya','06':'Ankara'}
+#Sözlük elemanlarına erişmek için anahtar değerinin adı tanımlanan değişkenden sonra konulacak köşeli parantezler içine yazılır.
+#print(plakalar['06'])
+
+#Sözlüklerde bir elemanın değerini değiştirmek için anahtar değerine karşılık gelecek şekilde tanımlandıktan sonra eşitlik işaretinin sağına yeni değer tanımlanır.
+#plakalar["06"] = "İstanbul"
+#print(plakalar["06"])
+
+#Sözlüklerde bir elemanın varlığını sorgulamak için in veya not in ifadelerini kullanabiliriz.
+#print("06" in plakalar)
+#print("34" not in plakalar)
+
+
+
+
+
+input()
+
